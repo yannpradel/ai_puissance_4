50de72f07d37fa658deebb56858cb040d34d8334
yannpradel <yann.pradel1@gmail.com>
2022-01-28 19:33:59 +0100

    change language

---
+++ include/Board.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+
+class Board
+{
+    public:
+        Board();
+
+    protected:
+
+    private:
+};
+
+#endif // BOARD_H
---
+++ include/Square.h
@@ -0,0 +1,16 @@
+#ifndef CASE_H
+#define CASE_H
+
+
+class Square
+{
+    public:
+        Square();
+        ~Square();
+
+    private:
+        int x;
+        int y;
+};
+
+#endif // CASE_H
